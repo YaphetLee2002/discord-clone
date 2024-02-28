7f71e99f92a21bf0789c06229e33f03d94a60b29
Yaphet <YaphetLee2002@qq.com>
2024-02-28 10:31:45 +0800

    Initial commit from Create Next App

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,15 +1,18 @@
 "use client"
 
+import axios from "axios";
 import * as z from "zod"
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
+import {useEffect, useState} from "react";
+
 import {
     Dialog,
     DialogContent,
-    DialogDescription, DialogFooter,
+    DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
 } from "@/components/ui/dialog"
 
 import {
@@ -23,10 +26,13 @@ import {
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { FileUpload } from "@/components/file-upload";
+import {useRouter} from "next/navigation";
+
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server name is required"
+        message: "Server name is required."
     }),
     imageUrl: z.string().min(1, {
         message: "Server image is required"
@@ -35,18 +41,37 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
 
+    const [isMounted, setIsMounted] = useState(false)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
     const form = useForm({
+        resolver: zodResolver(formSchema),
         defaultValues: {
-            resolver: zodResolver(formSchema),
             name: "",
-            imageUrl: ""
+            imageUrl: "",
         }
     })
 
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("/api/servers", values)
+            form.reset()
+            router.refresh()
+            window.location.reload()
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    if (!isMounted) {
+        return null
     }
 
     // @ts-ignore
@@ -65,13 +90,29 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                >
 
+                                </FormField>
                             </div>
 
                             <FormField
                                 control={form.control}
                                 name="name"
-                                render={({ filed }) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel
                                             className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
@@ -82,21 +123,19 @@ export const InitialModal = () => {
                                             <Input
                                                 disabled={isLoading}
                                                 className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                                placeholder="Enter a server name"
-                                                {...filed}
+                                                placeholder="Enter server name"
+                                                {...field}
                                             />
                                         </FormControl>
+                                        <FormMessage />
                                     </FormItem>
                                 )}
                             />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
-                            <Button disabled={isLoading}>
-
-
+                            <Button variant="primary" disabled={isLoading}>
+                                Create
                             </Button>
-
-
                         </DialogFooter>
                     </form>
                 </Form>
